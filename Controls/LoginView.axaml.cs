da99c993eec2bf36957709fcb374e0638f701656
Russell.Palma <russell.palma@amcsgroup.com>
2025-07-08 12:14:14 +0800

    lol

---
+++ Controls/LoginView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoPBI.ViewModels;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace AutoPBI;
+
+public partial class LoginView : Window
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButton_OnClick(object sender, PointerPressedEventArgs e)
+    {
+        ((MainViewModel)DataContext!).Login();
+        Close();
+    }
+}
