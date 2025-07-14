ac10b1a6e7ef962b6ca58fa87a0538bbec508335
simon.escano <simon.escano@amcsgroup.com>
2025-07-14 19:13:02 +0800

    Changed icons

--- Controls/IconButton.axaml.cs
+++ Controls/IconButton.axaml.cs
@@ -1,15 +1,85 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using Avalonia.Interactivity;
 using Avalonia.Media;
 
 namespace AutoPBI.Controls;
 
 public class IconButton : TemplatedControl
 {
+    static IconButton()
+    {
+        BackgroundProperty.Changed.AddClassHandler<IconButton>((control, args) =>
+        {
+            control.CoerceValue(HoverBackgroundProperty);
+        });
+        
+        BorderBrushProperty.Changed.AddClassHandler<IconButton>((control, args) =>
+        {
+            control.CoerceValue(HoverBorderBrushProperty);
+        });
+    }
+    private static IBrush? CoerceHoverBackground(AvaloniaObject instance, IBrush? value)
+    {
+        var iconButton = (IconButton)instance;
+        return value ?? iconButton.Background;
+    }
+    
+    private static IBrush? CoerceHoverBorderBrush(AvaloniaObject instance, IBrush? value)
+    {
+        var iconButton = (IconButton)instance;
+        return value ?? iconButton.BorderBrush;
+    }
+    
+    public static readonly StyledProperty<IBrush> HoverBackgroundProperty = AvaloniaProperty.Register<IconButton, IBrush>(
+        nameof(HoverBackground), 
+        defaultValue: null!,
+        coerce: CoerceHoverBackground!);
+
+    public IBrush HoverBackground
+    {
+        get => GetValue(HoverBackgroundProperty);
+        set => SetValue(HoverBackgroundProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush> HoverBorderBrushProperty = AvaloniaProperty.Register<IconButton, IBrush>(
+        nameof(HoverBorderBrush), 
+        defaultValue: null!,
+        coerce: CoerceHoverBorderBrush!);
+
+    public IBrush HoverBorderBrush
+    {
+        get => GetValue(HoverBorderBrushProperty);
+        set => SetValue(HoverBorderBrushProperty, value);
+    }
+    
+    public static readonly StyledProperty<object?> IconUnicodeProperty = AvaloniaProperty.Register<IconButton, object?>(
+        nameof(IconUnicode), false);
+
+    public object? IconUnicode
+    {
+        get => GetValue(IconUnicodeProperty);
+        set => SetValue(IconUnicodeProperty, value);
+    }
+    
+    public static readonly StyledProperty<object?> TextProperty = AvaloniaProperty.Register<IconButton, object?>(
+        nameof(Text), false);
+
+    public object? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly StyledProperty<Thickness> IconMarginProperty = AvaloniaProperty.Register<IconButton, Thickness>(
+        nameof(IconMargin), new Thickness(0, 0, 8, 0));
+
+    public Thickness IconMargin
+    {
+        get => GetValue(IconMarginProperty);
+        set => SetValue(IconMarginProperty, value);
+    }
+
     public static readonly StyledProperty<ICommand> CommandProperty = AvaloniaProperty.Register<IconButton, ICommand>(
         nameof(Command));
 
@@ -26,23 +96,5 @@ public class IconButton : TemplatedControl
     {
         get => GetValue(CommandParameterProperty);
         set => SetValue(CommandParameterProperty, value);
-    }
-    
-    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<IconButton, string>(
-        nameof(Text));
-
-    public string Text
-    {
-        get => GetValue(TextProperty);
-        set => SetValue(TextProperty, value);
-    }
-
-    public static readonly StyledProperty<string> PathProperty = AvaloniaProperty.Register<IconButton, string>(
-        nameof(Path));
-
-    public string Path
-    {
-        get => GetValue(PathProperty);
-        set => SetValue(PathProperty, value);
     }
 }
